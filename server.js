63fa4436ed47e1269d19821feeefb78b71350ab3
Rohit Singh <rohitdev124421@gmail.com>
2026-02-28 17:28:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require("./src/app")
+
+
+ const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
