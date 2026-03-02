681d474bdd669c364ea56ef8219e6e90f40c1fc8
Rohit Singh <rohitdev124421@gmail.com>
2026-03-02 10:40:56 +0530

    error not fixed

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const app = require("./src/app")
+// const dns = require("dns")
 const Dbconnect =  require("./src/config/database")
 const port = 3000
 Dbconnect()
 
 
 
-
+// dns.setServers(["1.1.1.1","8.8.8.8"])
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
-const exrpess = require("express");
 const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
@@ -7,56 +7,67 @@ exports.signup = async (req, res) => {
     const { name, email, password, age, gender } = req.body;
 
     const exist = await userModel.findOne({ email });
-
+    
     if (exist) {
-      return res.status(400).json({
-        message: "user alredy exist",
-      });
+      return res.status(400).json({ message: "User already exists" });
     }
+
     const hashpassword = await bcrypt.hash(password, 10);
-    const token =  jwt.sign(                //means signature kar deti h
-     {
-          id: user._id,
-          email: user.email
-     },
-     process.env.JWT_SECRET
-    )
-    res.cookie("jwt_token",token)
-    const user = new userModel.create({
+
+    const user = await userModel.create({
       name,
       email,
       password: hashpassword,
       age,
       gender,
-      token
     });
 
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
 
+    res.cookie("jwt_token", token, { httpOnly: true });
 
-     await user.save()
-    if (user) {
-      res.status(200).json({ message: "Signup successful" });
-    }
+    res.status(201).json({
+      message: "Signup successful",
+      token,
+    });
+
   } catch (error) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: error.message });
   }
 };
-
-
 exports.login = async (req, res) => {
-   try {
-   const {email, password} = req.body
-    
-   const EmailMacth = await userModel.findOne({email})
+  try {
+    const { email, password } = req.body;
 
-   if (!EmailMacth) return res.status(400).json({ message: "User not found" });
-    
-   const passwordIsmacth = await bcrypt.compare(password, user.password)
-   if (passwordIsmacth) return res.status(400).json({message: "user password incorrect"})
-    
-   res.json({ token, user: { name: user.name, email: user.email } });
-
-   } catch (error) {
-     res.status(500).json({ message: err.message });
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
-}
+
+    const passwordIsMatch = await bcrypt.compare(password, user.password);
+    if (!passwordIsMatch) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.cookie("jwt_token", token, { httpOnly: true });
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: { name: user.name, email: user.email },
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
