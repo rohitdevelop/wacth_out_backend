98a772a886a9e30ed09d8b2b33348a269dbe1957
Rohit Singh <rohitdev124421@gmail.com>
2026-03-15 17:36:27 +0530

    update routes for login and users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,36 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+// all users get method
+exports.users = async (req, res) => {
+  try {
+
+    const users = await userModel.find();
+
+    if (users.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No users found"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Users fetched successfully",
+      count: users.length,
+      users
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+      error: error.message
+    });
+  }
+};
+
+// singup post method
 exports.signup = async (req, res) => {
   try {
     const { name, email, password, age, gender } = req.body;
@@ -39,6 +69,8 @@ exports.signup = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// login post method
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,9 +2,11 @@ const express = require("express")
 
 const authRouter = express.Router()
 
-const {signup} = require("../controllers/user.controller")
+const {signup,login,users} = require("../controllers/user.controller")
 
 authRouter.post("/signup", signup)
+authRouter.post("/login", login)
+authRouter.get("/users", users)
 
 
 module.exports = authRouter
